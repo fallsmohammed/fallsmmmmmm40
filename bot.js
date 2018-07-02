94024e4b4ad96c699da2f379dd3ec1bb293e7b79
falls86 <40698133+falls86@users.noreply.github.com>
2018-07-02 20:31:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-@here
+
 client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('ايدي الروم');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
@@ -37,7 +37,6 @@ client.on('voiceStateUpdate', (old, now) => {
 });
 
 
- وبسيط للجافا سكريبت
 
 
 client.login(process.env.BOT_TOKEN);

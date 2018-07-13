85f51c73e0a02cc179d7d73945117b6aa23c7c9a
fallsmohammed <40745423+fallsmohammed@users.noreply.github.com>
2018-07-13 20:46:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,31 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 });
 
 
- 
+const devs = ['455519607432806410'];
+const adminprefix = "*";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+  });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
